45009f2958906f970db5120271ac96a2a60b05e3
Łukasz Lechowicz <lukasz.lechowicz@made.com>
2021-10-14 22:42:01 +0200

    things

--- zaqpy/pages/index.tsx
+++ zaqpy/pages/index.tsx
@@ -1,8 +1,10 @@
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +16,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -67,3 +69,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
